045d20e60a5ea527751a1f15cd059b52cc049c42
Alejandro <alejandro.medina@territorium.com>
2023-11-25 12:11:55 -0600

    Se agregan validaciones y correcciones

--- indeed_scraping.py
+++ indeed_scraping.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
-import random
+import re
 from datetime import datetime
 import pandas as pd
 import numpy as np
@@ -11,88 +11,98 @@ import undetected_chromedriver as uc
 from openpyxl import Workbook
 import sys
 import ssl
+import validators
 
-# La linea del SSL es necesaria para la ejecucion en MacOs
+# If you have an issue related to undetected_chromedriver go to undetected_chromedriver module
+# and edit line 799 more or less in file __init__.py and put the time.sleep(0.1) inside a try-except
+
+# This line about SSL is required to work properly on Mac OS
+# if you are working on Windows can comment this line
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# El error que tenia con undetected_chromedriver lo solucione editando 
-# el archivo __init__.py en la linea 799 agregué el try-except
 
 if __name__ == '__main__':
     
-    url = sys.argv[1] # Tomamos la URL pasada como argumento
+    if len(sys.argv) <= 1:
+        print("Es necesario pasar la URL como argumento.\nEjemplo: https://mx.indeed.com/jobs?q=php&l=&from=searchOnHP&vjk=ab8e2d4c5c516867")
+        exit()
     
-    browser = uc.Chrome(use_subprocess=True)
-    browser.get(url)
-    browser.implicitly_wait(10);
-
-    html = browser.page_source
-    soup = bs(html, 'html.parser')
-
-    browser.close()
-    browser.quit()
-
-    #########################################################
+    # URL as argument
+    url = sys.argv[1]
     
-    jobs = soup.find_all('td', {'class':'resultContent'})
-    
-    titles = []
-    links = []
-    companies = []
-    salaries = []
+    if validators.url(url):
 
-    for job in jobs:
+        browser = uc.Chrome(use_subprocess=True)
+        browser.get(url)
+        browser.implicitly_wait(10);
+
+        html = browser.page_source
+        soup = bs(html, 'html.parser')
+
+        browser.close()
+        browser.quit()
+
+        #########################################################
         
-        # Si no tiene salario especificado nos saltamos al siguiente
-        if job.find('div', {'class':'salary-snippet-container'}) == None:
-            continue
+        jobs = soup.find_all('td', {'class':'resultContent'})
         
-        title = job.find('h2', {'class':'jobTitle'}).text.strip()
-        link = job.find('a', {'class':'jcs-JobTitle'}).attrs['href']
-        link = 'https://mx.indeed.com' + link
-        company = job.find('span', {'data-testid':'company-name'}).text.strip()
-        salary = salary = job.find('div', {'class':'salary-snippet-container'}).text.strip()
-        salary = salary.replace('Hasta', '').replace('por mes', '').replace('por hora', '').replace('a', '-')
+        titles = []
+        links = []
+        companies = []
+        salaries = []
 
-        
-        titles.append(title)
-        links.append(link)
-        companies.append(company)
-        salaries.append(salary)
-        
-    
-    # Creamos el dataframe
-    df = pd.DataFrame(
-            {
-                "Empleo": titles, 
-                "Salario": salaries,
-                "Empresa": companies,
-                "Link": links 
-            }
-        )
-    
-    # df.to_excel("vacantes_indeed.xlsx", sheet_name="Sheet1", engine="xlsxwriter", index=False)
-    
-    now = datetime.now()
-    date_string = now.strftime('%Y%m%d')
-    time_string = now.strftime('%H%M%S')
-    now = f"{date_string}_{time_string}"
-    
-    # Crea un writer del excel de pandas usando XlsxWriter como motor
-    with pd.ExcelWriter('vacantes_indeed_'+ now +'.xlsx', engine='openpyxl') as writer:
-        # Escribimos el dataframe al excel
-        df.to_excel(writer, sheet_name='Sheet1', index=False)
-
-        # Accede al XlsxWriter workbook y los objectos worksheet
-        workbook = writer.book
-        worksheet = writer.sheets['Sheet1']
-
-        # Ancho de las columnas
-        width_measures = [40, 40, 40, 130]
-        
-        for col_num, value in enumerate(df.columns.values, 1):
-            column_letter = chr(64 + col_num)
-            worksheet.column_dimensions[column_letter].width = width_measures[col_num - 1]
+        for job in jobs:
             
-    print("Terminando proceso...")
+            # If salary is not public we jump next job
+            if job.find('div', {'class':'salary-snippet-container'}) == None:
+                continue
+            
+            title = job.find('h2', {'class':'jobTitle'}).text.strip()
+            link = job.find('a', {'class':'jcs-JobTitle'}).attrs['href']
+            link = 'https://mx.indeed.com' + link
+            company = job.find('span', {'data-testid':'company-name'}).text.strip()
+            salary = salary = job.find('div', {'class':'salary-snippet-container'}).text.strip()
+            salary = salary.replace('Hasta', '').replace('por mes', '').replace('por hora', '').replace('a', '-')
+
+            
+            titles.append(title)
+            links.append(link)
+            companies.append(company)
+            salaries.append(salary)
+            
+        
+        # Create DataFrame
+        df = pd.DataFrame(
+                {
+                    "Empleo": titles, 
+                    "Salario": salaries,
+                    "Empresa": companies,
+                    "Link": links 
+                }
+            )
+        
+        
+        now = datetime.now()
+        date_string = now.strftime('%Y%m%d')
+        time_string = now.strftime('%H%M%S')
+        now = f"{date_string}_{time_string}"
+        
+        with pd.ExcelWriter('vacantes_indeed_'+ now +'.xlsx', engine='openpyxl') as writer:
+            
+            df.to_excel(writer, sheet_name='Sheet1', index=False)
+
+            workbook = writer.book
+            worksheet = writer.sheets['Sheet1']
+
+            # Columns width
+            width_measures = [40, 40, 40, 130]
+            
+            for col_num, value in enumerate(df.columns.values, 1):
+                column_letter = chr(64 + col_num)
+                worksheet.column_dimensions[column_letter].width = width_measures[col_num - 1]
+                
+        print("Terminando proceso...")
+    
+    else:
+        print("El formato de la URL no es válido.")
         
